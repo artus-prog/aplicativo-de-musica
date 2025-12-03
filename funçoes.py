3f2b0c9e526ff6bf958777bff6a98378cbb9c37b
artus-prog <artusalves15@gmail.com>
2025-12-03 18:10:39 -0300

    musicas salvas e pagina nova
    Aperfeiçoei o salvamento de musicas e adicionei uma pagina que contem as funções separando do código original

---
+++ funçoes.py
@@ -0,0 +1,65 @@
+nome_playlists = []
+playlists = []
+musicas_salvas=[]
+usuario = None
+senha = None
+
+#Está função tem relações com as funções login_entrar e primeiro_acesso
+#A função desta é organizar os protocolos de login
+def login():
+   while True:
+      login_inicial=input("Você está acessando este aplicativo pela primeira vez? (sim/não)").strip().lower()
+      if login_inicial == "sim":
+         primeiro_acesso()
+         break
+
+      elif login_inicial in ("não","nao"):
+         login_entrar()
+         break
+
+      else:
+         print("Resposta inválida!")
+
+#A seguinte função serve para o caso de
+#o usuario estar fazendo seu primeiro acesso
+def primeiro_acesso():
+   usuario=input("Qual nome de usúario você deseja? ")
+   senha=input("Digite a senha desejada: ")
+   print(f"Seu nome de usuário é: {usuario} e sua senha é: {senha}")
+   with open("login", "w", encoding="utf-8") as arquivo:
+      arquivo.write(f"Usuário: {usuario}\n")
+      arquivo.write(f"Senha: {senha}")
+   return True
+
+#Esta função serve para os acessos 
+#seguintes do usuario que ja possui conta
+def login_entrar ():
+   while True:
+      usuário_tentativa = input("Digite seu login: ")
+      senha_tentativa = input("Digite sua senha: ")
+      if usuário_tentativa == usuario and senha_tentativa == senha:
+         print("Login aceito")
+         return True
+         break
+      else: 
+         print("Tente novamente")
+
+#Função a ser utilizada para salvar musicas fora de playlists
+def salvar_musica(música):
+   musicas_salvas.append(música)
+   escolha = input("Pressione enter para sair ou digite o nome de outra música: ")
+   if escolha == "":
+      print(f"Ok, Suas músicas salvas são: {musicas_salvas}")
+   else:
+      salvar_musica(escolha)
+
+#Ainda trabalhando
+def nova_playlist():
+      nova_playlist=bool(input("Você deseja adicionar uma nova playlist?"))
+      
+      if nova_playlist==True:
+         nome_playlist=input("Digite o nome da sua nova playlist: ")
+         playlists.append(nome_playlist)
+def escutarPlaylist(Playlist):
+   for i in Playlist:
+      input(f"Tocando: {i}, para pular pressione enter!")
--- main.py
+++ main.py
@@ -1,77 +1,23 @@
-nome_playlists = []
-playlists = []
-musicas_salvas=[]
-usuario = ""
-senha = ""
+import funçoes
 
-#Está função tem relações com as funções login_entrar e primeiro_acesso
-#A função desta é organizar os protocolos de login
-def login():
-   login_inicial=input("Você está acessando este aplicativo pela primeira vez? (sim/não)").strip().lower()
-
-   if login_inicial == "sim":
-      primeiro_acesso()
-      return False
-
-   elif login_inicial in ("não,nao"):
-      login_entrar()
-   else:
-      print("Resposta inválida!")
-      return False
-
-#A seguinte função serve para o caso de
-#o usuario estar fazendo seu primeiro acesso
-def primeiro_acesso():
-   usuario=input("Qual nome de usúario você deseja? ")
-   senha=input("Digite a senha desejada: ")
-   print(f"Seu nome de usuário é: {usuario} e sua senha é: {senha}")
-   return True
-
-#Esta função serve para os acessos 
-#seguintes do usuario que ja possui conta
-def login_entrar ():
-   while True:
-      usuário_tentativa = input("Digite seu login: ")
-      senha_tentativa = input("Digite sua senha: ")
-      if usuário_tentativa == usuario and senha_tentativa == senha:
-         print("Login aceito")
-         return True
-         break
-      else: 
-         print("Tente novamente")
-def salvar_musica():
-   escolha = input("Você deseja adicionar uma música a sua lista de músicas salvas? ").strip().lower()
-   if escolha == "sim":
-      musica_nova = input("Digite o nome da sua música nova e o autor seguindo o seguinte modelo: Amor Bandido-Oruam. ")
-      musicas_salvas.append(musica_nova)
-      print(f"Você adicionou: {musica_nova} as suas músicas salvas")
-   elif escolha == "nao" or escolha == "não":
-      print("Ok")
-   else:
-      ("Opção inválida")
-   print(f"Suas músicas salvas são: {musicas_salvas}")
-def nova_playlist():
-      nova_playlist=bool(input("Você deseja adicionar uma nova playlist?"))
-      
-      if nova_playlist==True:
-         nome_playlist=input("Digite o nome da sua nova playlist: ")
-         playlists.append(nome_playlist)
-
-def main_site():
-   login()
+funçoes.login()
+while True:
+   print("Bem vindo a sla aplicativo de música")
    print("""___MENU___
 1. Acessar músicas salvas
 2. Acessar suas playlists 
 """)
-   escolha = int(input("O que deseja fazer: "))
-   if escolha == 1:
-      if len(musicas_salvas) == 0:
-         print("Você não possui músicas salvas")
-         salvar_musica()
+   acesso_menu = int(input("O que deseja fazer: "))
+   if acesso_menu == 1:
+      while True:
+         print("""___músicas salvas___
+1. Ouvir múscicas salvas
+2. Acessar músicas salvas""")
+         if len(funçoes.musicas_salvas) == 0:
+            print("Você não possui músicas salvas")
+         else:
+            print(f"Suas músicas salvas são: {funçoes.musicas_salvas}")
+         break
+      
 
-
-
-while True:
-   print("Bem vindo a sla aplicativo de música")
-   main_site()
    break
